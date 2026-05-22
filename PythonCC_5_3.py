0ef504ace388970fa4cd6ab4448df453701d871b
otanimex <alejandro.rechy@gmail.com>
2026-05-22 08:59:14 -0600

    Include PythonCC_5_3.py

---
+++ PythonCC_5_3.py
@@ -0,0 +1,19 @@
+#########################################################################
+#   Python Crash Course                                                 #
+#   Chapter 5 If Statements                                             #
+#   Excersice 3 Alien Colors 1                                          #
+#   By Otanimex                                                         #
+#   Instructions:                                                       #
+#   Imagine an alien was just shot down in a game. Create a variable    #
+#   called alien_color and assing it a value of 'gree', 'yellow', or    #
+#   'red'.                                                              #
+#########################################################################
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You've earned 5 points")
+    alien_color="yellow"
+else:
+    alien_color="red"
+
