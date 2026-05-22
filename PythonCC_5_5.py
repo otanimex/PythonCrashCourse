a41d49d671b54e38228de51b6eb43a77529ab8b0
otanimex <alejandro.rechy@gmail.com>
2026-05-22 13:53:52 -0600

    PythonCC_5_5.py added

---
+++ PythonCC_5_5.py
@@ -0,0 +1,22 @@
+#########################################################################
+#   Python Crash Course                                                 #
+#   Chapter 5 If Statements                                             #
+#   Exercise 5 Alien Colors 3                                           #
+#   By Otanimex                                                         #
+#   Instructions:                                                       #
+#   Turn your if-else chain from Exercise 5-4 into an if-elif-else      #
+#   chain.                                                              #
+#########################################################################
+
+#You're supposed to not choose other than green, yellow and red.
+alien_color = "red"
+
+if alien_color == "green":
+    print("You've earned 5 points")
+elif alien_color == "yellow":
+    print("You've earned 10 points")
+elif alien_color == "red":
+    print("You've earned 10 points")
+else:
+    print("You shouldn't be reading this")
+
