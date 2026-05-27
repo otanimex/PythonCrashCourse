5d1bbdd6e2d0fb5b3c250ab7bb028c45b6deabc9
otanimex <alejandro.rechy@gmail.com>
2026-05-26 21:25:16 -0600

    First commenting correction

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,10 @@
+#########################################################################
+#   Python Crash Course                                                 #
+#   Chapter 2  Variables and simple data types                          #
+#   Exercise 1  Simple message                                          #
+#   By Otanimex                                                         #
+#   Instructions:                                                       #
+#   Store a message in a variable, and then print that message.         #
+#########################################################################
+
 print("Hola mundo, otro más pero esta vez ya se lo que hago")
--- Hello_World2.py
+++ Hello_World2.py
@@ -1,3 +1,15 @@
+#########################################################################
+#   Python Crash Course                                                 #
+#   Chapter 2  Variables and simple data types                          #
+#   Exercise 2  Simple messages                                         #
+#   By Otanimex                                                         #
+#   Instructions:                                                       #
+#   Store a message in a variable, and then print that message.         #
+#   Then change tha value of your variable message, and then print the  #
+#   new message.                                                        #
+#########################################################################
+
+
 mensaje="Hola mundo, otro más pero esta vez ya se lo que hago"
 
 print(mensaje)
