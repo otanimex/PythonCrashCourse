61b30106391bfd01507266273a5dbf46f3b39957
otanimex <alejandro.rechy@gmail.com>
2026-07-11 19:59:20 -0600

    Added 8.1 only

---
+++ PythonCC_8_1.py
@@ -0,0 +1,15 @@
+#########################################################################
+#   Python Crash Course                                                 #
+#   Chapter 8 Functions                                                 #
+#   Exercise 1 Message                                                  #
+#   By Otanimex                                                         #
+#   Instructions:                                                       #
+#   Write a function called display_message() that prints one sentence  #
+#   telling everyone what you are learning about in this chapter.       #
+#   Call the function, and make sure the message display correctly.     #
+#########################################################################
+
+def display_message():
+    print("Pues aca toca aprender de funciones banda")
+
+display_message()
